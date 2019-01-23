86494d488cc7d4e0769731c09b19be498faa44a4
BaliT530 <46794101+BaliT530@users.noreply.github.com>
2019-01-23 14:46:20 -0500

    Update Hello.java

--- src/src/Hello.java
+++ src/src/Hello.java
@@ -3,6 +3,7 @@ public class Hello
 {
     public static void main(String[] args)
     {
+        //Reads in the user input and then prints out that input - Edit by Trevor
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Enter your name:");
         String name = keyboard.nextLine();
